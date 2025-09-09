9bd0ec733edcab9a2cb5b380d1af69c725125533
Neel-H-Prajapati <prajapatinimisha853@gmail.com>
2025-09-09 10:43:14 +0530

    added styling in client logo

--- src/Components/Client.js
+++ src/Components/Client.js
@@ -1,14 +1,19 @@
-import React from 'react'
-import Avatar from 'react-avatar'
-const Client = ({key,username}) => {
-  return (
-    <div className="client">
-      <div className="clientwrap">
-        <span className="clienticon"><Avatar name={username} size={50} round="15px" /></span>
-        <span className="clientname">{username}</span>
-      </div>
-    </div>
-  )
-}
+import React from "react";
+import Avatar from "react-avatar";
 
-export default Client
+const Client = ({ username }) => {
+  return (
+    <div className="d-flex align-items-center mb-2">
+      <Avatar
+        name={username}
+        size="35"
+        round={true}
+        textSizeRatio={2}
+        className="me-2"
+      />
+      <span className="text-secondary">{username}</span>
+    </div>
+  );
+};
+
+export default Client;
