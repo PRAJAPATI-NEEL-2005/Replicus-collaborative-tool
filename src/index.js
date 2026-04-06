5f2bafb7498a5d42e538e587f211897876139480
PRAJAPATI NEEL <prajapatinimisha853@gmail.com>
2026-04-06 19:24:44 +0530

    added routes and auth context

--- src/index.js
+++ src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import AuthProvider from "./context/AuthContext"; // ✅ IMPORT
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   
+    <AuthProvider>   {/* 🔥 THIS WAS MISSING */}
     <App />
+  </AuthProvider>
  
 );
 
