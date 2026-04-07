f01f593a530cb25ecb3650b43da0a3532162c620
PRAJAPATI NEEL <prajapatinimisha853@gmail.com>
2026-04-07 10:22:19 +0530

    mailsend of otp is added

---
+++ utils/sendMail.js
@@ -0,0 +1,20 @@
+const nodemailer = require("nodemailer");
+
+const sendMail = async (email, otp) => {
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASS, // App password
+    },
+  });
+
+  await transporter.sendMail({
+    from: process.env.EMAIL_USER,
+    to: email,
+    subject: "Replicus OTP Verification",
+    html: `<h2>Your OTP is: ${otp}</h2><p>Valid for 5 minutes</p>`
+  });
+};
+
+module.exports = sendMail;
