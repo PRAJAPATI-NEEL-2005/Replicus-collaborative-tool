de723b9f54d54723e0831d0f2b80115a28475f36
PRAJAPATI NEEL <prajapatinimisha853@gmail.com>
2026-04-09 18:41:06 +0530

    cede run data model done

---
+++ models/CodeRunLog.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+const CodeRunLogSchema = new mongoose.Schema({
+  roomId: { type: String, required: true },
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' }, // Assuming you use ObjectId for users
+  username: { type: String, required: true },
+  email: { type: String },
+  language: { type: String, required: true },
+  timestamp: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model("CodeRunLog", CodeRunLogSchema);
