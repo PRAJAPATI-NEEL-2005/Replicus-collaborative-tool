30c65541bbb24db981acfef312b0f21c7939e1ec
PRAJAPATI NEEL <prajapatinimisha853@gmail.com>
2026-04-09 18:43:40 +0530

    model update

--- models/CodeRunLog.js
+++ models/CodeRunLog.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const CodeRunLogSchema = new mongoose.Schema({
   roomId: { type: String, required: true },
-  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' }, // Assuming you use ObjectId for users
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' }, 
   username: { type: String, required: true },
   email: { type: String },
   language: { type: String, required: true },
