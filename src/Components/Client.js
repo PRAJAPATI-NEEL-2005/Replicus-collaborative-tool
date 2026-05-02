e984d0550e869beb5fe0b69e5d938fc4665d9384
PRAJAPATI NEEL <prajapatinimisha853@gmail.com>
2026-05-02 12:59:28 +0530

    client list updated

--- src/Components/Client.js
+++ src/Components/Client.js
@@ -1,19 +1,20 @@
 import React from "react";
 import Avatar from "react-avatar";
 
-const Client = ({ username }) => {
+const Client = ({ username, isSelf }) => {
   return (
     <div className="d-flex align-items-center mb-2">
       <Avatar
-        name={username}
+        name={username} // The avatar now only sees "John"
         size="35"
         round={true}
         textSizeRatio={2}
         className="me-2"
       />
-      <span className="text-secondary">{username}</span>
+      <span className="text-secondary">
+        {username}{isSelf ? " (You)" : ""} {/* Appends "(You)" only in the UI text */}
+      </span>
     </div>
   );
 };
-
 export default Client;
